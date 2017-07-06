ad22e583dac08f5f5360fc4d1103695928976963
eintk <HinagikuKatsura303@163.com>
2017-07-06 09:29:56 +0800

    sss

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+from .models import Post
+from django.shortcuts import redirect
+from django.template.loader import get_template
+
 
 # Create your views here.
+def homepage(request):
+    template=get_template('index.html')
+    posts=Post.objects.all()
+    now=datetime.now()
+    html=template.render(locals())
+    return HttpResponse(html)
+
+
+def showpost(request,slug):
+    template=get_template('post.html')
+    try:
+        post=Post.objects.get(slug=slug)
+        if post !=None:
+            html=template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
